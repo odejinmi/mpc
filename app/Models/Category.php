ba6f6ebee86279b613651868bbc479ee07750f5f
videx <45696129+odejinmi@users.noreply.github.com>
2024-12-20 17:40:20 +0100

    category model

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -11,7 +11,7 @@ class Category extends Model
 {
     use HasFactory;
 
-    protected $fillable = ['name'];
+    protected $fillable = ['name','parent_id','priority','position','slug'];
 
     protected $casts = [
         'name' => 'string',
